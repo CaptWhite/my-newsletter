f40d8e375cd856fe491c4228e49a7342783c31d0
CaptWhite <captwhite57@gmail.com>
2023-11-27 17:54:50 +0100

    newsletter

---
+++ src/data/news.js
@@ -0,0 +1,14 @@
+export const news = [
+  {
+    url: 'https://csuc.us17.list-manage.com/track/click?u=f87308e90af909dc7f919ecc2&amp;id=a1d90e4230&amp;e=3b89e9e576',
+    image: 'https://www.csuc.cat/sites/default/files/2023-10/cora-portal.png',
+    title: 'EEEEntra en funcionament CORA, el portal per promoure la ciència oberta a Catalunya',
+    summary: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus est, dolores illum veritatis voluptate iure. Nobis saepe iusto totam cum sapiente necessitatibus veniam veritatis amet eum minus recusandae blanditiis excepturi iure nostrum accusantium expedita sequi dolore ut harum molestias, sit, debitis eos culpa! Quibusdam, numquam doloremque harum non repudiandae sapiente.'
+  },
+  {
+    url: 'https://csuc.us17.list-manage.com/track/click?u=f87308e90af909dc7f919ecc2&amp;id=a1d90e4230&amp;e=3b89e9e576',
+    image: 'https://www.csuc.cat/sites/default/files/2023-10/cora-portal.png',
+    title: 'Entra en funcionament CORA, el portal per promoure la ciència oberta a Catalunya',
+    summary: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus est, dolores illum veritatis voluptate iure. Nobis saepe iusto totam cum sapiente necessitatibus veniam veritatis amet eum minus recusandae blanditiis excepturi iure nostrum accusantium expedita sequi dolore ut harum molestias, sit, debitis eos culpa! Quibusdam, numquam doloremque harum non repudiandae sapiente.'
+  }
+] 
